7e529a11f1d43cd86b316ba53cba373aba5a3081
Nanda Gunawan <54890927+Gugun09@users.noreply.github.com>
2021-07-08 19:31:36 +0800

    Update edu.sh

--- edu.sh
+++ edu.sh
@@ -3,7 +3,7 @@
 # ==============================
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/Gugun09/edu/main/edu.py
+wget -q -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/Gugun09/sshws/main/edu.py
 chmod +x /usr/local/bin/edu-proxy
 
 # Installing Service
