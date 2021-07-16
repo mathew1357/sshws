301ac5886a848480c3215d9686b15cc975471256
Nanda Gunawan <54890927+Gugun09@users.noreply.github.com>
2021-07-16 19:14:58 +0800

    Update edu.sh

--- edu.sh
+++ edu.sh
@@ -19,7 +19,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy 8880
+ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy
 Restart=on-failure
 
 [Install]
